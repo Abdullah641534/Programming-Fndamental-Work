44b39f8e95f603f86551cf9e0f5d1df2bfc4ebc8
Abdullah <am2408066@gmail.com>
2026-02-20 10:51:06 -0700

    Added Week 2 Assignment

---
+++ Week 2/Task10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main()
+{
+system(" color 47 ");
+cout<<"\n\n\n\n";
+cout<<"   .....                ooooo    ooooo          ...........\n";
+cout<<"   ......              o     o  o     o         ..........\n";
+cout<<"   .......                ooo   o     o         .........\n";
+cout<<"   ........             oo      o     o         ........\n";
+cout<<"    ........           ooooooo   ooooo  th      .......\n";
+cout<<"      .......        .......................    ......\n";
+cout<<"        ......         C  E  N  T  U  R  Y      .....\n";
+cout<<"          .....      .......................    ....\n";
+cout<<"             ...     ooooo ooooo o   o ooooo    ...\n";
+cout<<"              ..       o   o      o o    o      ..\n";
+cout<<"             _||_      o   oooo    o     o     _||_\n";
+cout<<"            |    |     o   o      o o    o    |    |\n";
+cout<<"   _________|____|___  o   ooooo o   o   o ___|____|_________";
+}
